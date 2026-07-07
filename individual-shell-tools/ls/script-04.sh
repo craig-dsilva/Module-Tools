df52eeec3d210a85a77e2d5db5c8832f16f96abf
Craig D'Silva <craigdsilva@proton.me>
2026-07-07 20:36:06 +0100

    Fix ls

--- individual-shell-tools/ls/script-04.sh
+++ individual-shell-tools/ls/script-04.sh
@@ -24,4 +24,4 @@ echo "Second exercise (sorted oldest to newest):"
 # TODO: Write a command which does the same as above, but sorted in the opposite order (oldest first).
 # The output should be a list of names in this order, one per line: helper-2.txt, helper-1.txt, helper-3.txt.
 
-ls child-directory -tr
+ls child-directory -tr1
